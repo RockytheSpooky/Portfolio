389207ada296c0b67953c36aa67c0cce4c6ff21b
RockytheSpooky <sreematkandasamy6@gmail.com>
2025-12-11 22:15:57 -0600

    script.js changed

--- script.js
+++ script.js
@@ -1,1 +1,19 @@
+// toggle for the dark and light mode in gallery
+function toggleGalleryMode() {
+  const body = document.body;
+  body.classList.toggle("dark-mode");
+}
 
+// the form validation for the contact page
+function validateForm() {
+  let name = document.getElementById("name").value;
+  let msg = document.getElementById("message").value;
+
+  if (name === "" || msg === "") {
+    alert("Please fill out all required fields.");
+    return false;
+  }
+
+  alert("Message sent!");
+  return true;
+}
